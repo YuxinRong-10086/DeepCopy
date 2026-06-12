c9a03a6a7887e7cb94f770cf0cd6db13bd1a866d
YuxinRong-10086 <evelynrong11@gmail.com>
2026-06-12 00:46:42 -0700

    Update ClassNotRegisteredException.java

--- ClassNotRegisteredException.java
+++ ClassNotRegisteredException.java
@@ -1,5 +1,9 @@
 public class ClassNotRegisteredException extends Exception {
 
+        /**
+        * Constructs an exception with a custom message
+        * @param message exception message
+        */
         public ClassNotRegisteredException(String message) {
            super(message);
     }
